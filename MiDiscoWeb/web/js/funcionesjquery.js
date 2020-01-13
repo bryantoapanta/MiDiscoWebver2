995f4395c3d9d5a75497a5a6713f9ab585f10921
albeorte96 <46744901+albeorte96@users.noreply.github.com>
2020-01-13 10:30:30 +0100

    Add files via upload

---
+++ MiDiscoWeb/web/js/funcionesjquery.js
@@ -0,0 +1,34 @@
+/*var x = "";
+x = $(document);
+x.ready(inicializarEventos);
+
+function presionModificar() {
+	  alert("Usted accederá a modificar sus datos");
+}
+
+function presionDetalles() {
+	  alert("Usted accederá a comprobar los detalles");
+}
+
+function inicializarEventos() {
+	  $("#modificar").click(presionModificar);
+	  $("#detalles").click(presionDetalles);
+}
+
+var x;
+x = $(document);
+x.ready(inicializarEventos);
+
+function presionCambiaColor() {
+  var x;
+  x=$(".users");
+  x.css("color","#ff0000")
+  x.css("background-color","#ffff00")
+  x.css("font-family","courier")
+}
+
+function inicializarEventos() {
+  var x;
+  x=$(".users");
+  x.click(presionCambiaColor)  
+}*/
