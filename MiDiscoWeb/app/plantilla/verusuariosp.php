93ae516bac4db9a1c976ac8694636bb3a897cc4f
albeorte96 <46744901+albeorte96@users.noreply.github.com>
2020-01-15 11:51:21 +0100

    Update verusuariosp.php

--- MiDiscoWeb/app/plantilla/verusuariosp.php
+++ MiDiscoWeb/app/plantilla/verusuariosp.php
@@ -13,16 +13,21 @@ $auto = $_SERVER['PHP_SELF'];
 // identificador => Nombre, email, plan y Estado
 ?>
 <?php foreach ($usuarios as $clave => $datosusuario) : ?>
-
-		
 		
 		<tr>
 			<td class="users"><?= $clave ?></td> 
-	<?php for  ($j=0; $j < count($datosusuario); $j++) :?>
-     <td><?=$datosusuario[$j] ?></td>
-	<?php endfor;?>
-<td class="borrador"  ><a href="#"
-				onclick="confirmarBorrar('<?= $datosusuario[0]."','".$clave."'"?>);">&#9760;&#9851;</a></td>
+			<?php for  ($j=0; $j < count($datosusuario); $j++) :?>
+     		<?php 
+     		if ($j==0 || $j==1 || $j==3){
+     		    echo "<td class=\"ocultos\">";
+     		    echo $datosusuario[$j];
+     		}else{
+     		    echo "<td>";
+     		    echo $datosusuario[$j];
+     		}
+     		 ?></td>
+			<?php endfor;?>
+			<td class="borrador"  ><a href="#" onclick="confirmarBorrar('<?= $datosusuario[0]."','".$clave."'"?>);">&#9760;&#9851;</a></td>
 			<td class="modificacion"><a href="<?= $auto?>?orden=Modificar&id=<?= $clave ?>">&#9998;</a></td>
 			<td class="detalle"><a href="<?= $auto?>?orden=Detalles&id=<?= $clave?>">&#9776;</a></td>
 		</tr>
